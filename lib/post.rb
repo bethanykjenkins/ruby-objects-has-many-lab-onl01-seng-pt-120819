685b50f459581b1e45f2be4edf0ab63a01534efa
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-02-04 01:50:25 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,26 @@
+class Post
+  attr_accessor :title, :author
+
+@@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << title
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def title
+    @title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name 
+    end
+  end
+end
